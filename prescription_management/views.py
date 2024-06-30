4f6bf3f84f882fc4aa5003dc2b2b504459cfa620
michaelcyril <michaelcyril71@gmail.com>
2024-06-30 20:51:22 +0300

    commit

--- prescription_management/views.py
+++ prescription_management/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from user_management.models import User
+import uuid
 
 
 class PrescriptionView(APIView):
@@ -12,21 +13,20 @@ class PrescriptionView(APIView):
     get_serializer_class = PrescriptionGetSerializer
     post_serializer_class = PrescriptionPostSerializer
     user_model = User
-    @staticmethod
-    def post(request):
+    def post(self, request):
         data = request.data
         try:
             serialized = PrescriptionPostSerializer(data=data)
             isvalid = serialized.is_valid()
             if isvalid:
-                serialized.save()
+                prescription_id = "PRESC" + str(uuid.uuid4().int)[:6]
+                serialized.save(prescription_id=prescription_id)
                 return Response({"save": True})
             return Response({"save": False, "errors": serialized.errors})
         except:
             return Response({"save": False, "errors": "Error Occured"})
 
-    @staticmethod
-    def get(request):
+    def get(self, request):
         query_type = request.GET.get("query_type")
         if query_type == "client_prescription":
             try:
@@ -34,7 +34,8 @@ class PrescriptionView(APIView):
                 prescription_status = request.GET.get("prescription_status")
                 user = self.user_model.objects.get(id=client_id)
                 if prescription_status:
-                    queryset = self.model.objects.filter(user=user, status=prescription_status, active=True)
+                    queryset = self.model.objects.filter(user=user)
+                    queryset = queryset.filter(status=prescription_status).filter(active=True)
                     serialized = self.get_serializer_class(instance=queryset, many=True)
                     return Response(serialized.data)
                 else:
@@ -45,7 +46,7 @@ class PrescriptionView(APIView):
                 return Response([])
         elif query_type == "pharmacist_prescription":
             prescription_status = request.GET.get("prescription_status")
-            if order_status:
+            if prescription_status:
                 queryset = self.model.objects.filter(status=prescription_status, active=True)
                 serialized = self.get_serializer_class(instance=queryset, many=True)
                 return Response(serialized.data)
@@ -58,27 +59,27 @@ class PrescriptionView(APIView):
 
 
 class DeleteUpdatePrescription(APIView):
-    @staticmethod
-    def post(request):
+    def post(self, request):
         try:
             data = request.data
+            print(data)
             prescription = Prescription.objects.get(id=data['id'])
-            # prescription.litre = data['litre']
+            prescription.description = data['description']
+            prescription.total_price = int(data['total_price'])
             prescription.save()
             return Response({"update": True})
         except:
-            return Response({"update": False, "error": "Water Does Not Exists"})
+            return Response({"update": False, "error": "Prescription Does Not Exists"})
 
 
-    @staticmethod
-    def get(request):
+    def get(self, request):
         try:
             id = request.GET.get("id")
             prescription = Prescription.objects.get(id=id)
             prescription.delete()
             return Response({"delete": True})
         except Prescription.DoesNotExist:
-            return Response({"delete": False, "error": "Water Does Not Exists"})
+            return Response({"delete": False, "error": "Prescription Does Not Exists"})
 
 
 class ChangePrescriptionStatusView(APIView):
@@ -90,7 +91,7 @@ class ChangePrescriptionStatusView(APIView):
             prescription = self.model.objects.get(id=data['id'])
             prescription.status = data['status_to']
             prescription.save()
-            return Response({"chenge": True})
+            return Response({"change": True})
         except self.model.DoesNotExist:
             return Response({"change": False})
 
